432f4c3e7f365d756ca2ede631e55202950808cd
John Moen <entity.john@gmail.com>
2020-01-17 13:04:29 -0600

    ungitignore build/ and push up

---
+++ build/index.es.js
@@ -0,0 +1,29 @@
+function ___$insertStyle(css) {
+  if (!css) {
+    return;
+  }
+  if (typeof window === 'undefined') {
+    return;
+  }
+
+  var style = document.createElement('style');
+
+  style.setAttribute('type', 'text/css');
+  style.innerHTML = css;
+  document.head.appendChild(style);
+  return css;
+}
+
+import React from 'react';
+
+___$insertStyle(".test-component {\n  background-color: white;\n  border: 1px solid black;\n  padding: 16px;\n  width: 360px;\n  text-align: center;\n}\n.test-component .heading {\n  font-size: 64px;\n}\n.test-component.test-component-secondary {\n  background-color: black;\n  color: white;\n}");
+
+var TestComponent = function (_a) {
+    var theme = _a.theme;
+    return (React.createElement("div", { className: "test-component test-component-" + theme },
+        React.createElement("h1", { className: "heading" }, "I'm the test component"),
+        React.createElement("h2", null, "Made with love by Harvey")));
+};
+
+export { TestComponent };
+//# sourceMappingURL=index.es.js.map
