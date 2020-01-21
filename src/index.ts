afebcbe2745948fbf4fbff363b59f8fcdefbe733
John Moen <entity.john@gmail.com>
2020-01-21 00:55:57 -0600

    declare the module, see if error goes away: ts(7016) for terra-comp-lib

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import TestComponent from "./test-component/test-component";
 import { rootReducer, store, RootState, getInitialData, getVRPin } from './store'
-
 export { TestComponent, rootReducer, store, RootState, getInitialData, getVRPin };
