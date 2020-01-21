7b5ccacdb9a5f738b452f380bfa2a255d20198be
John Moen <entity.john@gmail.com>
2020-01-21 00:40:48 -0600

    forgot to remove HMR on module store

--- build/index.js
+++ build/index.js
@@ -4377,12 +4377,6 @@ var rootReducer = combineReducers({
 var store = configureStore({
     reducer: rootReducer
 });
-if (process.env.NODE_ENV === "development" && module.hot) {
-    module.hot.accept("./rootReducer", function () {
-        var newRootReducer = require("./rootReducer").default;
-        store.replaceReducer(newRootReducer);
-    });
-}
 
 exports.TestComponent = TestComponent;
 exports.getInitialData = getInitialData;
--- src/store/index.ts
+++ src/store/index.ts
@@ -8,12 +8,6 @@ import { getVRPin } from './vrPinSlice'
 const store = configureStore({
   reducer: rootReducer
 });
-if (process.env.NODE_ENV === "development" && module.hot) {
-  module.hot.accept("./rootReducer", () => {
-    const newRootReducer = require("./rootReducer").default;
-    store.replaceReducer(newRootReducer);
-  });
-}
 
 export type AppDispatch = typeof store.dispatch;
 export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
